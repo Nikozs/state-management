3c8823dab92db29de1cd4039210fd75c72d2acc5
Nikozs <niko.purola@gmail.com>
2020-11-15 16:02:22 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const passport = require("./utils/pass");
 const session = require("express-session");
 const app = express();
 const port = 3000;
@@ -12,6 +13,16 @@ app.use(bodyParser.json());
 const username = "foo";
 const password = "bar";
 
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/form");
+  }
+};
+
+
 app.use(cookieParser());
 app.use(
   session({
@@ -21,6 +32,9 @@ app.use(
     cookie: { maxAge: 60000 },
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -38,25 +52,20 @@ app.get("/form", (req, res) => {
   console.log("Cookies: ", req.cookies);
 });
 
-app.get("/secret", (req, res) => {
-  console.log("Cookies: ", req.cookies);
-  if (req.session.logged) {
-    res.render("secret");
-  } else {
-    res.redirect("/form");
-  }
-  console.log("Cookies: ", req.cookies);
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
 });
 
-app.post("/login", (req, res) => {
-  if (req.body.username === username && req.body.password === password) {
-    req.session.logged = true;
+app.post("/login", passport.authenticate("local", { failureRedirect: "/form" }), (req, res) => {
+    console.log("success");
     res.redirect("/secret");
-  } else {
-    res.redirect("/form");
   }
-  console.log("Cookies: ", req.cookies);
-});
+);
+
+app.get("/logout", function(req, res){
+    req.logout()
+    res.redirect("/");
+  });
 
 app.get("/setCookie/:clr", (req, res) => {
   res.cookie("color", req.params.clr);
